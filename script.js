337a62faf88e6cae2ca5b9e18556bb7ef27f9140
Dai Taniguchi <D.taniguchi0403@gmail.com>
2023-01-28 23:53:06 +0100

    script.js

--- script.js
+++ script.js
@@ -97,64 +97,61 @@ let userScore = 0;
 let comScore = 0;
 
 // play up to 5 rounds
-for (let i = 1; i <= 5; i++) {
-  const choice = [`rock`, `paper`, `scissors`];
+const choice = [`rock`, `paper`, `scissors`];
 
-  function checkInput(input) {
-    return choice.includes(input);
+function checkInput(input) {
+  return choice.includes(input);
+}
+function userChoice() {
+  let input = prompt("Choose betwee Rock, Paper and Scisscors").toLowerCase();
+  while (input === null) {
+    input = prompt("Please type, choose betwee Rock, Paper and Scisscors");
   }
-  function userChoice() {
-    let input = prompt("Choose betwee Rock, Paper and Scisscors").toLowerCase();
+
+  let check = checkInput(input);
+  while (check === false) {
+    console.log(`wrong input`);
+    input = prompt(
+      "Wrong input, choose betwee Rock, Paper and Scisscors"
+    ).toLowerCase();
+
     while (input === null) {
       input = prompt("Please type, choose betwee Rock, Paper and Scisscors");
     }
-
-    let check = checkInput(input);
-    while (check === false) {
-      console.log(`wrong input`);
-      input = prompt(
-        "Wrong input, choose betwee Rock, Paper and Scisscors"
-      ).toLowerCase();
-
-      while (input === null) {
-        input = prompt("Please type, choose betwee Rock, Paper and Scisscors");
-      }
-      check = checkInput(input);
-    }
-    return input;
+    check = checkInput(input);
   }
-  let playerSelection = userChoice();
-  const getComputerChoice = function () {
-    return choice[Math.floor(Math.random() * choice.length)];
-  };
-  const computerSelection = getComputerChoice();
-  let win = `You win. ${firstUp(playerSelection)} beats ${computerSelection}.`;
-  let tie = `It's tie`;
-  let lose = `You lose. ${firstUp(
-    computerSelection
-  )} beats ${playerSelection}.`;
-  // playround
-  const playRound = function (playerSelection, computerSelection) {
-    if (
-      (playerSelection === `rock` && computerSelection === `scissors`) ||
-      (playerSelection === `paper` && computerSelection === `rock`) ||
-      (playerSelection === `scissors` && computerSelection === `paper`)
-    ) {
-      userScore = userScore + 1;
-      return win;
-    } else if (playerSelection === computerSelection) {
-      return tie;
-    } else {
-      comScore += 1;
-      return lose;
-    }
-  };
-
-  console.log(playRound(playerSelection, computerSelection));
-  let left = 5 - i;
-  console.log(userScore, comScore, `${left} rounds left`);
-  // console.log(`${left} rounds left`);
+  return input;
 }
+let playerSelection = userChoice();
+const getComputerChoice = function () {
+  return choice[Math.floor(Math.random() * choice.length)];
+};
+const computerSelection = getComputerChoice();
+let win = `You win. ${firstUp(playerSelection)} beats ${computerSelection}.`;
+let tie = `It's tie`;
+let lose = `You lose. ${firstUp(computerSelection)} beats ${playerSelection}.`;
+// playround
+const playRound = function (playerSelection, computerSelection) {
+  if (
+    (playerSelection === `rock` && computerSelection === `scissors`) ||
+    (playerSelection === `paper` && computerSelection === `rock`) ||
+    (playerSelection === `scissors` && computerSelection === `paper`)
+  ) {
+    userScore = userScore + 1;
+    return win;
+  } else if (playerSelection === computerSelection) {
+    return tie;
+  } else {
+    comScore += 1;
+    return lose;
+  }
+};
+
+console.log(playRound(playerSelection, computerSelection));
+let left = 5 - i;
+console.log(userScore, comScore, `${left} rounds left`);
+// console.log(`${left} rounds left`);
+
 let finalScore = `The final score is you:${userScore}, computer:${comScore}`;
 if (userScore > comScore) {
   console.log(`Congratulation, ${finalScore} `);
